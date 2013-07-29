9c04c87040919010dc08028d11fce3dafa5fa246
Ynon Perek <ynonperek@gmail.com>
2013-07-29 22:03:04 +0300

    another typo

--- root/static/list.js
+++
@@ -1,7 +0,0 @@
-$('tr').on('click', function() {
-  var name = $(this).attr('data-name');
-  var url = window.location.href.replace(/\/$/,'');
-
-  window.location.href = url + '/edit/' + name;
-
-});
