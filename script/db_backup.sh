b90356e3eccabd9a4d57c906f66a18d041d44efb
Ynon Perek <ynonperek@gmail.com>
2014-01-10 09:09:01 +0200

    add comments to explain how to add a new db schema

--- script/db_backup.sh
+++ script/db_backup.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# When adding a new DB, be sure to run the following in mysql
+# (replace moti with your new db schema)
+# grant SELECT, LOCK TABLES, SHOW VIEW, EVENT, TRIGGER ON `moti`.* to 'backup'@'localhost';
+# grant usage on moti.* to 'backup'@'localhost';
+
 mysqldump -B fbb kalisher moti| bzip2 > /tmp/db.sql.bz2
 echo "DB backup attached" | mail -s "DB Backup" -a /tmp/db.sql.bz2 kalisher@gmail.com
 
