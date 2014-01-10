0e0edf34e7f8c9418dbdbbb9d63913424dae3219
Ynon Perek <ynonperek@gmail.com>
2014-01-10 09:03:14 +0200

    add moti db

--- script/db_backup.sh
+++ script/db_backup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-mysqldump -B fbb kalisher | bzip2 > /tmp/db.sql.bz2
+mysqldump -B fbb kalisher moti| bzip2 > /tmp/db.sql.bz2
 echo "DB backup attached" | mail -s "DB Backup" -a /tmp/db.sql.bz2 kalisher@gmail.com
 
